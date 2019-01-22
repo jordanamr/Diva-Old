c4b11b49dc566c39911d8244ec17953437d2d618
Aquazus <me@aquazus.fr>
2019-01-22 22:02:48 +0100

    ProtocolHandler update

---
+++ diva-protocol/src/main/java/fr/aquazus/diva/protocol/DivaProtocol.java
@@ -0,0 +1,5 @@
+package fr.aquazus.diva.protocol;
+
+public interface DivaProtocol {
+    String version = "1.29.1";
+}
--- diva-protocol/src/main/java/fr/aquazus/diva/protocol/ProtocolHandler.java
+++
@@ -1,6 +0,0 @@
-package fr.aquazus.diva.protocol;
-
-public interface ProtocolHandler {
-    String version = "1.29.1";
-    boolean handlePacket(String packet);
-}
