36470ccc1e0887e9e103d68fcde32254d61b378c
Aquazus <me@aquazus.fr>
2019-02-02 23:52:26 +0100

    Database file update

--- diva-database/diva_auth.sql
+++ diva-database/diva_auth.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : MariaDB
+ Source Server         : Localhost
  Source Server Type    : MariaDB
- Source Server Version : 100135
+ Source Server Version : 100137
  Source Host           : localhost:3306
  Source Schema         : diva_auth
 
  Target Server Type    : MariaDB
- Target Server Version : 100135
+ Target Server Version : 100137
  File Encoding         : 65001
 
- Date: 20/01/2019 05:28:39
+ Date: 02/02/2019 23:49:58
 */
 
 SET NAMES utf8mb4;
@@ -34,13 +34,12 @@ CREATE TABLE `accounts`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username`(`username`) USING BTREE,
   UNIQUE INDEX `nickname`(`nickname`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of accounts
 -- ----------------------------
-INSERT INTO `accounts` VALUES (1, 'test', 'test', 'test', 'Supprimer ?', 'oui', -1, 3, 0);
-INSERT INTO `accounts` VALUES (2, 'test2', 'test2', 'test2', 'Supprimer ?', 'oui', -1, 1, 0);
+INSERT INTO `accounts` VALUES (1, 'test', 'test', 'test', 'Supprimer ?', 'oui', -1, 1, 0);
 
 -- ----------------------------
 -- Table structure for characters
@@ -49,11 +48,19 @@ DROP TABLE IF EXISTS `characters`;
 CREATE TABLE `characters`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `account_id` int(11) NOT NULL,
-  `server` int(11) NOT NULL,
+  `server_id` int(11) NOT NULL,
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `level` mediumint(3) NOT NULL DEFAULT 1,
+  `class` tinyint(2) NOT NULL DEFAULT 1,
+  `gender` tinyint(1) NOT NULL DEFAULT 0,
+  `gfx_id` int(11) NOT NULL DEFAULT 1010,
+  `color1` int(8) NOT NULL DEFAULT -1,
+  `color2` int(8) NOT NULL DEFAULT -1,
+  `color3` int(8) NOT NULL DEFAULT -1,
+  `is_merchant` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`, `account_id`) USING BTREE,
   UNIQUE INDEX `name`(`name`) USING BTREE,
-  UNIQUE INDEX `account_id`(`account_id`) USING BTREE
+  UNIQUE INDEX `id`(`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -81,7 +88,7 @@ DROP TABLE IF EXISTS `servers`;
 CREATE TABLE `servers`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `population` tinyint(1) NOT NULL DEFAULT 1,
+  `completion` tinyint(1) NOT NULL DEFAULT 1,
   `p2p` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 901 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
@@ -89,6 +96,6 @@ CREATE TABLE `servers`  (
 -- ----------------------------
 -- Records of servers
 -- ----------------------------
-INSERT INTO `servers` VALUES (900, 'Test', 1, 0);
+INSERT INTO `servers` VALUES (900, 'Test', 0, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
